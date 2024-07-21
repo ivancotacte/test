639f025f89a342821aa7d280f26382fbcf5d2830
Ivan Cotacte <cotactearmenion@gmail.com>
2024-07-21 18:52:51 +0800

    test

--- ivancotacte.js
+++ ivancotacte.js
@@ -1,6 +1,4 @@
 const got = require('got');
-const moment = require("moment-timezone");
-const encode = require('encodeurl');
 
 let config = {
     stars_repo : (process.env.REPO)?process.env.REPO:'ivancotacte/test'
@@ -27,11 +25,11 @@ let get_users = async function() {
 module.exports = {
     matchWord: 'TEST_REPO',
     transforms: {
-      /* Match <!-- PUNTORIGEN:START (LAST_UPDATE) --> */
+      /* Match <!-- TEST_REPO:START (LAST_UPDATE) --> */
         async LAST_UPDATE(content, options) {
             let date = require('date-and-time');
             let format = (options.format)?options.format:'DD-MM-YYYY HH:mm';
-            let now_f = date.format(new Date(), format, true).replaceAll('-','--')+' (GMT 8)'; // gmt 0
+            let now_f = date.format(new Date(), format, true).replaceAll('-','--')+' (GMT 0)'; // gmt 0
             let encode = require('encodeurl');
             let encoded = encode(now_f);
             return `![last_update](https://img.shields.io/badge/last%20update-${encoded}-blue)`;
